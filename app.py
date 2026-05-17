40f11f596e5b56849de8094ca08c8bfa97fc7fd3
fernandoflores <u202221580@upc.edu.pe>
2026-05-16 23:23:11 -0500

    qr sitio 2

--- app.py
+++ app.py
@@ -809,7 +809,7 @@ class FacturaXMLtoPDF:
         pdf.set_font("Arial", 'B', 8)
         pdf.set_xy(qr_x, qr_y + qr_width)
         pdf.cell(qr_width, 4, "SITIO WEB", 0, 0, 'C')
-                
+
         num_documento = self.data.get('numero_factura', 'N/A')
         fecha = self.data.get('fecha_emision', 'N/A')
         guia_remision = self.data.get('cliente_guia', 'N/A')
@@ -1109,7 +1109,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MANCHESTER PRO - Conversor XML/CSV a PDF</title>
+    <title>Conversor XML a PDF</title>
     <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
